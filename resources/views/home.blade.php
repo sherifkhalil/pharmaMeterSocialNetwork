d72ac1c56c8e823a764146ab350b5636a98cf945
ahmedsalama51 <ahmed.salama1679@gmail.com>
2016-05-29 23:35:51 +0200

    add new design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,118 +2,181 @@
 
 @section('content')
  <div id="latest-blog">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                           <div class="heading-section">
-                                
-                                <div class='form-group form-group-sm'>
-                                    <form>
-                                        <div class='col-xs-12'>
-                                            <textarea  class='form-control' name='post'  placeholder="add new post"></textarea>
-                                            <hr>
-                                            <input  class='col-xs-2 pull-right btn btn-sm btn-primary' type='submit' name='Add' />
-                                        </div>
-
-                                    </form>
-                                     <hr>
-                                </div>
-                            </div>
-
-                            <!-- <div class='form-group form-group-sm'>
-                                <div class='col-xs-offset-2 col-xs-4'>
-                                    <input type='submit' name='loginbtn' class='btn btn-primary input-xs' value='Publish'/>
-                                </div>-->
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-8 col-sm-12">
+    <div class="container">  
+        {{-- posts --}}
+        <div class="blog-content">
+            <div class="wrap">
+                <div class="blog-content-left">
+                    <div class="blog-articals">
+                        <div class="heading-section">
                         
-
-                            <div class="blog-post">
-
-                                <img src="images/1.png" alt="" id="profile"/>
-                                <span> <a href="#"> Username</a> </span>
-                                <hr/>
-                                <p>Sed egestas tincidunt mollis. Suspendisse rhoncus vitae enim et faucibus. Ut dignissim nec arcu nec hendrerit. Sed arcu  sagittis vel diam in, malesuada malesuada risus. Aenean a sem leoneski.</p>
-                                <div class="blog-thumb">
-                                <img src="images/blogpost1.jpg" alt="" />
+                        <div class='form-group form-group-sm'>
+                            <form>
+                                <div class='col-xs-12'>
+                                    <textarea  class='form-control' name='post'  placeholder="add new post"></textarea>
+                                    <hr>
+                                    <input  class='col-xs-2 pull-right btn btn-sm btn-primary' type='submit' name='Add' value="Publish"/>
                                 </div>
 
-                                <a href="#" > Up </a>
-                                <a href="#" > Comment </a>
-                                <input type="text"  name='comment'  placeholder="add new comment"/>
-
-                                
-                            </div>
+                            </form>
+                            <div class="clearfix"> </div>
                         </div>
 
-                        <div class="col-md-8 col-sm-12">
-                        
-
-                            <div class="blog-post">
-
-                                <img src="images/1.png" alt="" id="profile"/>
-                                <span> <a href="#"> Username</a> </span>
-                                <hr/>
-                                <p>Sed egestas tincidunt mollis. Suspendisse rhoncus vitae enim et faucibus. Ut dignissim nec arcu nec hendrerit. Sed arcu  sagittis vel diam in, malesuada malesuada risus. Aenean a sem leoneski.</p>
-                                <div class="blog-thumb">
-                                <img src="images/blogpost2.jpg" alt="" />
-                                </div>
-
-                                <a href="#" > Up </a>
-                                <a href="#" > Comment </a>
-                                <input type="text"  name='comment'  placeholder="add new comment"/>
-
-                                
-                            </div>
-                        </div>
-                      
-                        <div class="col-md-4 col-sm-6">                           
-                            <div class="blog-post" id="sub">
-                            
-                               <div id="uname">
-                                   <img src="images/1.png" alt="" id="profile"/>
-                                    <span > <a href="#"> Username</a> </span>
-                                    <span id="follow"> <a href="#"> Follow </a> </span>
-                               </div>
-                               <div id="uname">
-                                   <img src="images/1.png" alt="" id="profile"/>
-                                    <span > <a href="#"> Username</a> </span>
-                                    <span id="follow"> <a href="#"> Follow </a> </span>
-                               </div>
-                               <div id= "add"> <h3> People you may know ?  </h3></div>
-                               <div id= "ad"> <h3> Advertisments </h3>  </h3>
-                                <div class="blog-thumb">
-                                <img src="images/blogpost4.jpg" alt="" />
-                                </div>
-                               </div>
-                                
-                                </div>
-                        </div>
-                        </div>
-                      
-                        <div class="col-md-4 col-sm-6">
-                            <div class="blog-post">
-                                
-                             </div>
-                        </div>
-
-
-            <div id="testimonails">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            
-                        </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <div class="testimonails-slider">
-                              
+                    <div class="blog-artical">
+                        <div class="blog-artical-basicinfo">
+                            <ul>
+                                <li class="post-date"><p><span> 05</span><label>Nov,2014</label></p></li>
+                                <li class="categoery"> <span> </span></li>
+                                <li class="artlick"><a href="#"><span> </span> <i>90</i></a></li>
+                                <li class="art-comment"><a href="#"><span> </span> <i>50</i></a></li>
+                                <div class="clearfix"> </div>
+                            </ul>
+                        </div>
+                        <div class="blog-artical-info">
+                            <div class="blog-artical-info-img">
+                                <a href="#"><img src="images/slide1.jpg" title="post-name"></a>
+                            </div>
+                            <div class="blog-artical-info-head">
+                                <h2><a href="#">Blog image and text post</a></h2>
+                                <ul>
+                                    <li><span> </span>by <a href="#">John Deo</a></li>
+                                    <li><span> </span><a href="#">Lorem</a>,<a href="#">Ipsum</a>,<a href="#">printer</a>,<a href="#">unknown</a></li>
+                                    <div class="clearfix"> </div>
+                                </ul>
+                            </div>
+                            <div class="blog-artical-info-text">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<a href="#">[...]</a></p>
                             </div>
                         </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                    <div class="blog-artical">
+                        <div class="blog-artical-basicinfo">
+                            <ul>
+                                <li class="post-date"><p><span> 05</span><label>Nov,2014</label></p></li>
+                                <li class="categoery"> <span> </span></li>
+                                <li class="artlick"><a href="#"><span> </span> <i>90</i></a></li>
+                                <li class="art-comment"><a href="#"><span> </span> <i>50</i></a></li>
+                                <div class="clearfix"> </div>
+                            </ul>
+                        </div>
+                        <div class="blog-artical-info">
+                            <div class="blog-artical-info-img">
+                                <a href="#"><img src="images/slide2.jpg" title="post-name"></a>
+                            </div>
+                            <div class="blog-artical-info-head">
+                                <h2><a href="#">Blog image and text post</a></h2>
+                                <ul>
+                                    <li><span> </span>by <a href="#">John Deo</a></li>
+                                    <li><span> </span><a href="#">Lorem</a>,<a href="#">Ipsum</a>,<a href="#">printer</a>,<a href="#">unknown</a></li>
+                                    <div class="clearfix"> </div>
+                                </ul>
+                            </div>
+                            <div class="blog-artical-info-text">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<a href="#">[...]</a></p>
+                            </div>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                    <div class="blog-artical">
+                        <div class="blog-artical-basicinfo">
+                            <ul>
+                                <li class="post-date"><p><span> 05</span><label>Nov,2014</label></p></li>
+                                <li class="categoery"> <span> </span></li>
+                                <li class="artlick"><a href="#"><span> </span> <i>90</i></a></li>
+                                <li class="art-comment"><a href="#"><span> </span> <i>50</i></a></li>
+                                <div class="clearfix"> </div>
+                            </ul>
+                        </div>
+                        <div class="blog-artical-info">
+                            <div class="blog-artical-info-img">
+                                <a href="#"><img src="images/slide3.jpg" title="post-name"></a>
+                            </div>
+                            <div class="blog-artical-info-head">
+                                <h2><a href="#">Blog image and text post</a></h2>
+                                <ul>
+                                    <li><span> </span>by <a href="#">John Deo</a></li>
+                                    <li><span> </span><a href="#">Lorem</a>,<a href="#">Ipsum</a>,<a href="#">printer</a>,<a href="#">unknown</a></li>
+                                    <div class="clearfix"> </div>
+                                </ul>
+                            </div>
+                            <div class="blog-artical-info-text">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<a href="#">[...]</a></p>
+                            </div>
+                        </div>
+                        <div class="clearfix"> </div>
                     </div>
                 </div>
+                <!---start-blog-pagenate---->
+            <div class="blog-pagenat">
+                <ul>
+                    <li><a class="frist" href="#"> <span> </span></a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a class="last" href="#"> <span> </span></a></li>
+                    <div class="clearfix"> </div>
+                </ul>
             </div>
+            <!---//End-blog-pagenate---->
+                </div>
+                <div class="twitter-weights">
+                    <div class="blog-content-right">
+                        <div class="b-search">
+                            <form>
+                                <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+                                <input type="submit" value="">
+
+                            </form>
+                        </div>                         
+                    <div class="blog-post" id="sub">
+                    </br>
+                     <div id= "add"> <h3> People you may know ?  </h3></div></br>
+                       <div class="uname">
+                           <img src="images/1.png" alt="" id="profile"/>
+                            <span > <a href="#"> Username</a> </span>
+                            <span id="follow"> <a href="#"> Follow </a> </span>
+
+                       </div>
+                       <div class="uname">
+                           <img src="images/1.png" alt="" id="profile"/>
+                            <span > <a href="#"> Username</a> </span>
+                            <span id="follow"> <a href="#"> Follow </a> </span>
+                       </div>
+                        <a class="twittbtn" href="#">See all users</a>
+                    </div>
+                    <!---//End-twitter-weight---->
+                    <!---- start-tag-weight---->
+                    <div class="b-tag-weight">
+                        <h3>Tags Weight</h3>
+                        <ul>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">consectetur</a></li>
+                            <li><a href="#">dolore</a></li>
+                            <li><a href="#">aliqua</a></li>
+                            <li><a href="#">sit amet</a></li>
+                            <li><a href="#">ipsum</a></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">consectetur</a></li>
+                            <li><a href="#">dolore</a></li>
+                            <li><a href="#">aliqua</a></li>
+                            <li><a href="#">sit amet</a></li>
+                            <li><a href="#">ipsum</a></li>
+                        </ul>
+                    </div>
+                    <!---- //End-tag-weight---->
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+            
+            </div>
+        </div>
+        </div>
+        <!-- /Blog -->
+         {{-- end of posts --}}
+            
+    </div>
+            
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,110 +1,115 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>KHAMSAT</title>
-<link rel="icon" href="{{ asset('images/icon.png')}}">
-<link href="{{ asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
-<link href="{{ asset('css/style.css')}}" rel="stylesheet" type="text/css" media="all" />
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css')}}">
-
-<link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}">
-<link rel="stylesheet" href="{{ asset('css/font-awesome.css')}}">
-<link rel="stylesheet" href="{{ asset('css/templatemo_style.css')}}">
-<link rel="stylesheet" href="{{ asset('css/templatemo_misc.css')}}">
-<link rel="stylesheet" href="{{ asset('css/flexslider.css')}}">
-<link rel="stylesheet" href="{{ asset('css/testimonails-slider.css')}}">
-
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Voguish Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
-<script type="publiclication/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,700' rel='stylesheet' type='text/css'> -->
-<script src="{{ asset('js/vendor/jquery-1.11.0.min.js')}}"></script>
-<script src="{{ asset('js/vendor/jquery.gmap3.min.js')}}"></script>
-<script src="{{ asset('js/plugins.js')}}"></script>
-<script src="{{ asset('js/main.js')}}"></script>
-<script src="{{ asset('js/responsiveslides.min.js')}}"></script>
-<script>
-    $(function () {
-      $("#slider").responsiveSlides({
-        auto: true,
-        nav: true,
-        speed: 500,
-        namespace: "callbacks",
-        pager: true,
-      });
-    });
-    
-  </script>
+<title>PHarmaMeter Social Network </title>
+        <link href="{{ asset('css/bootstrap.css')}}" rel='stylesheet' type='text/css' />
+        <script src="{{ asset('js/jquery.min.js')}}"></script>
+         <!-- Custom Theme files -->
+        <link href="{{ asset('css/style.css')}}" rel='stylesheet' type='text/css' />
+         <!-- Custom Theme files -->
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <script type="application/x-javascript">
+          addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }
+         </script>
+         <!-- webfonts -->
+         <link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
+          <!-- webfonts -->
+        <script src="{{ asset('js/vendor/jquery-1.11.0.min.js')}}"></script>
+        <script src="{{ asset('js/vendor/jquery.gmap3.min.js')}}"></script>
+        <script src="{{ asset('js/plugins.js')}}"></script>
+        <script src="{{ asset('js/main.js')}}"></script>
+        <script src="{{ asset('js/responsiveslides.min.js')}}"></script>
+        <script>
+            $(function () {
+              $("#slider").responsiveSlides({
+                auto: true,
+                nav: true,
+                speed: 500,
+                namespace: "callbacks",
+                pager: true,
+              });
+            });
+            
+          </script>
     
 </head>
 <body>
 <header>
-                <div id="top-header">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8">
-                                <div class="home-account">
-                                    <a href="#">Home</a>
-                                    <a href="#">My account</a>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                
-                            </div>
-                        </div>
+<!-- container -->
+            <!-- header -->
+            <div class="header header-border">
+                <div class="container">
+                    <!-- logo -->
+                    <div class="logo">
+                        <a href="index.html">
+                        Pharma Medical
+                        {{-- <img src="images/logo.png" title="medicalpluse" /> --}}
+                        </a>
                     </div>
-                </div>
-                <div id="main-header">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="logo">
-                                    <h3 id="logo"> Pharma </h3>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="main-menu">
-                                    <ul>
-                                        <li><a href="/">Home</a></li>
-                                        <li><a href="/about">About Us</a></li>
-                                    @if (Auth::guest())
-                                        <li><a href="{{ url('/login') }}">Login</a>
-                                        </li>
-                                        <li><a href="{{ url('/register') }}">Register</a></li>
-                                    @else
-                                        
-                                        <li><a href="/users/{{ Auth::user()->id }}" > <img width="30px" height="20px" src="{{ Auth::user()->image }}" alt=""> {{ Auth::user()->name }} </a>
-                                        <!-- {{ Auth::user()->name }} -->
-                                        <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                                           
-                                    @endif
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="search-box">  
-                                    <form name="search_form" method="get" class="search_form">
-                                        <input id="search" type="text" />
-                                        <input type="submit" id="search-button" />
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+                    <!-- logo -->
+                    <!-- top-nav -->
+                    <div class="top-nav">
+                        <span class="menu"> </span>
+                        <ul>
+                            <li><a href="{{ url('/') }}">Home</a></li>
+                            <li><a href="{{ url('/') }}">About</a></li>
+                            <li><a href="{{ url('/') }}">Services</a></li>
+                        @if (Auth::guest())
+                            <li><a href="{{ url('/login') }}">Login</a>
+                            </li>
+                            <li><a href="{{ url('/register') }}">Register</a></li>
+                        @else
+                            <li><a href="/users/{{ Auth::user()->id }}" >
+                                <img width="30px" height="20px" src="{{ Auth::user()->image }}" alt=""> {{ Auth::user()->name }} </a>
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                               
+                        @endif
+                        </ul>
                     </div>
+                    <!-- top-nav -->
+                        <!-- script-for-nav -->
+                    <script>
+                        $(document).ready(function(){
+                            $("span.menu").click(function(){
+                                $(".top-nav ul").slideToggle(300);
+                            });
+                        });
+                    </script>
+                    <!-- script-for-nav -->
+                    <div class="clearfix"> </div>
                 </div>
-            </header>
+            </div>
+            <!-- /header -->
+        </div>
+        <!-- /container -->
+        </div>
+        <!-- Blog -->
+        <div class="blog">
+            <div class="container-fluied">
 
-
+           
 <!-- header -->
     @yield('content')
 
 <!-- footer sector -->
-        <div class="footer">
+        </div><!-- /container -->
+    </div><!-- /Blog -->        
+    {{-- <!-- team-grids-caption -->
+    <div class="team-grids-caption">
+        <div class="container">
+            <div class="team-grids-caption-left">
+                <h4>He point of using Lorem Ipsum is that</h4>
+                <p>as opposed to using Many desktop publishing packages and web page editors now use </p>
+            </div>
+            <div class="team-grids-caption-right">
+                <a class="team-btn" href="contact.html">Contact</a>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div> --}}
+<!-- team-grids-caption -->
+<!-- footer -->
+    <div class="footer navbar navbar-default navbar-static-bottom">
             <div class="col-md-3 foot-1">
                 <h4>Latest Posts</h4>
                 <ul>
@@ -151,5 +156,4 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
-</html>
+
